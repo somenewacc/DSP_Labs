e9af7682f77db54cc965fa020074c99f0f443dbc
somenewacc <62895232+somenewacc@users.noreply.github.com>
2021-04-05 23:57:01 +0300

    codestyle improvements

--- Lab2/CreateSimplePlot.m
+++ Lab2/CreateSimplePlot.m
@@ -18,8 +18,8 @@ function [] = CreateSimplePlot( y, varargin )
     varargin_str = cellfun( @(x) num2str(x), varargin, 'UniformOutput', 0 );
 
     %% Draw plot in subplot %%
-    if ismember('subplot', varargin_str)
-        subplot_values_idx = find(strcmp(varargin_str, 'subplot')) + 1;
+    if ismember( 'subplot', varargin_str )
+        subplot_values_idx = find( strcmp( varargin_str, 'subplot' ) ) + 1;
         subplot_values = varargin{subplot_values_idx};
         if length(subplot_values) == 3
             a = subplot_values(1);
@@ -32,10 +32,11 @@ function [] = CreateSimplePlot( y, varargin )
     end
 
     %% Define custom x instead of '0:1:length(y) - 1' %%
-    if ismember('custom_x', varargin_str)
-        x_idx = find(strcmp(varargin_str, 'custom_x')) + 1;
-        if isnumeric(varargin{x_idx}) && length(varargin{x_idx}) == length(y)
-            x = varargin{x_idx};
+    if ismember( 'custom_x', varargin_str )
+        x_idx = find( strcmp( varargin_str, 'custom_x' ) ) + 1;
+        x_tmp = varargin{x_idx};
+        if isnumeric( x_tmp ) && length( x_tmp ) == length(y)
+            x = x_tmp;
         else
             fprintf('Problem with x value, using default!\n')
             x = 0:1:length(y) - 1;
@@ -45,17 +46,17 @@ function [] = CreateSimplePlot( y, varargin )
     end
 
     %% Stemmed plot %%
-    if ismember('stem', varargin_str)
-        stem(x, y, '.b')
+    if ismember( 'stem', varargin_str )
+        stem( x, y, '.b' )
         hold on
-        plot(x, y, '.r-')
+        plot( x, y, '.r-' )
     else
-        plot(x, y)
+        plot( x, y )
     end
 
     %% Add title to plot %%
-    if ismember('title', varargin_str)
-        title_idx = find(strcmp(varargin_str, 'title')) + 1;
+    if ismember( 'title', varargin_str )
+        title_idx = find( strcmp( varargin_str, 'title' ) ) + 1;
         plot_title = varargin{title_idx};
         title( plot_title, 'Interpreter', 'none' )
     end
